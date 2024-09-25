96e682893ad88a042165d4f2c8db4d6c1c779670
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-09-25 06:14:59 +0000

    multer middleware

--- backend/middleware/multer.middleware.js
+++ backend/middleware/multer.middleware.js
@@ -1,11 +1,11 @@
 import multer from "multer"
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/public/uploads')
+        cb(null, "./public/uploads/")
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 })
 
-export const upload = multer({ storage: storage })
+export const upload = multer({ storage })
