1973b9cd8bc7748abb64f0ff0f726068e964292b
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-09-28 17:43:37 +0000

    upload and delete on cloudinary

--- backend/cloudinary.js
+++ backend/cloudinary.js
@@ -1,32 +1,39 @@
 import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
+import dotenv from "dotenv"
+dotenv.config()
 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
 
 const uploadOnCloudinary = async (filepath) => {
-    cloudinary.config({
-        cloud_name: process.env.cloud_name,
-        api_key: process.env.api_key,
-        api_secret: process.env.api_secret
-    });
-
-    if (!filepath) {
-        fs.unlink(filepath , ()=>{})
-        return null
+    try {
+        if (!filepath) return null
+        const response = await cloudinary.uploader.upload(filepath, { resource_type: "image" })
+        fs.unlink(filepath, () => { })
+        const information = {
+            secure_url: response.secure_url,
+            resource_type: response.resource_type,
+            public_id: response.public_id,
+            type: response.type
+        }
+        return information
+    } catch (error) {
+        fs.unlink(filepath)
+        return error.message
     }
-
-    const response = await cloudinary.uploader.upload(filepath, { resource_type: "image" })
-    const information = {
-        secure_url: response.secure_url,
-        resource_type: response.resource_type,
-        public_id: response.public_id,
-        type: response.type
-    }
-    fs.unlink(filepath , ()=>{}) 
-    return information
 }
 
 const deleteFromCloudinary = async (public_id, resource_type) => {
-    await cloudinary.uploader.destroy(public_id, { resource_type, invalidate: true })
+    try {
+        if (!public_id) return null
+        await cloudinary.uploader.destroy(public_id, { resource_type, invalidate: true })
+    } catch (error) {
+        return error.message
+    }
 }
 
 export {
--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,11 @@ import cookieParser from "cookie-parser"
 import { connectDb } from "./db/db.js"
 import userRoutes from "./routes/user.routes.js"
 
-dotenv.config({
-    path:'./.env'
-})
+dotenv.config()
 const PORT = process.env.PORT
 const app = express()
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
