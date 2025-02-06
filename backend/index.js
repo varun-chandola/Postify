b36644f9a2de5d3555e7dfb8c68442cfa1de7c3b
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2025-02-06 18:41:33 +0000

    .env frontend url

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config()
 const PORT = process.env.PORT
 const app = express()
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: `${process.env.FRONTEND_URL}`,
     credentials: true
 }))
 app.use(express.json())
