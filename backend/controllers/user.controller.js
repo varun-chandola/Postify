fd659c650d9724be42033f48126be049386b7866
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2025-02-06 18:11:31 +0000

    added few more controllers

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,16 +1,19 @@
-import mongoose from "mongoose"
 import { uploadOnCloudinary, deleteFromCloudinary } from "../cloudinary.js"
 import jwt from "jsonwebtoken"
 import { User } from "../model/user.model.js"
 import { Blog } from "../model/blog.model.js"
+import { userCollection } from "../model/collection.model.js"
 
 export const signup = async (req, res) => {
     try {
         const { username, password, email } = req.body
         const avatarLocalPath = req.file?.path
-        const avatar = await uploadOnCloudinary(avatarLocalPath)
+        let avatar = ''
+        if (avatarLocalPath) {
+            avatar = await uploadOnCloudinary(avatarLocalPath)
+        }
 
-        if (!(username && password && email))
+        if (!username || !password || !email)
             return res.status(409).json({
                 msg: "all fields are mandatory"
             })
@@ -28,7 +31,6 @@ export const signup = async (req, res) => {
                 msg: "Username Taken"
             })
 
-
         const newUser = await User.create({
             username,
             password,
@@ -36,10 +38,16 @@ export const signup = async (req, res) => {
             avatar: avatar.secure_url
         })
 
-        return res.status(200).json({
-            msg: "new user registered",
-            newUser
+        const token = jwt.sign({
+            id: newUser?._id,
+            username
+        }, process.env.jwt_secret)
+
+        return res.status(200).cookie('token', token).json({
+            msg: "User Registration Successful",
+            token
         })
+
     } catch (error) {
         return res.status(500).json({
             message: "Error Signing Up",
@@ -50,8 +58,8 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     try {
-        const { username, password } = req?.body
-        if (!(username && password))
+        const { username, password } = req.body
+        if (!username || !password)
             return res.status(409).json({ msg: "Both Fields are mandatory" })
 
         const user = await User.findOne({ username })
@@ -62,15 +70,15 @@ export const login = async (req, res) => {
         if (!(await user.isPasswordCorrect(password)))
             return res.status(409).json({ msg: "Incorrect Password" })
 
-        const token = jwt.sign({ _id: user?._id, username }, process.env.jwt_secret)
+        const token = jwt.sign({
+            id: user?._id,
+            username
+        }, process.env.jwt_secret)
 
-        res
-            .status(200)
-            .cookie('token', token)
-            .json({
-                msg: "login successfull",
-                token
-            })
+        res.status(200).cookie('token', token).json({
+            msg: "Login successful",
+            token
+        })
 
     } catch (error) {
         return res.status(500).json({
@@ -82,7 +90,9 @@ export const login = async (req, res) => {
 
 export const allBlogs = async (req, res) => {
     try {
-        const allBlogs = await Blog.find({}).populate('owner', 'username , avatar')
+        const allBlogs = await Blog.find({}).sort({
+            createdAt: -1
+        }).select('-createdAt -updatedAt').populate('owner', "avatar  username").select('-parentCollection')
         return res.status(200).json({
             msg: "All blogs",
             allBlogs,
@@ -98,25 +108,27 @@ export const allBlogs = async (req, res) => {
 export const getPostById = async (req, res) => {
     try {
         const { blogId } = req.params
-        const blog = await Blog.findById(blogId).populate('owner', 'username avatar')
+        const blog = await Blog.findById(blogId).populate('owner', 'username avatar').populate('parentCollection', 'collectionName')
 
-        return res.status(200).json({ blog })
-    } catch (error) {
         return res.status(200).json({
+            blog,
+        })
+    } catch (error) {
+        return res.status(404).json({
             msg: "Error Fetching this blog",
             err: error.message
         })
     }
 }
 
-export const userBlogs = async (req, res) => {
+export const userProfile = async (req, res) => {
     try {
-        const yourBlogs = await User.findOne({
-            _id: req.user._id,
-        }).select('blogs avatar username').populate("blogs", "image title content tags likes")
+        const userProfile = await Blog.find({
+            owner: req?.user?.id
+        }).populate('parentCollection', 'collectionName')
 
         return res.json({
-            yourBlogs
+            userProfile
         })
 
     } catch (error) {
@@ -126,8 +138,63 @@ export const userBlogs = async (req, res) => {
     }
 }
 
+export const createCollection = async (req, res) => {
+    try {
+        const { collection } = req.body
+        console.log('collection', collection)
+        const nameTaken = await userCollection.findOne({
+            collectionOwner: req.user?.id,
+            collectionName: collection
+        })
+
+        if (nameTaken)
+            return res.status(409).json({
+                msg: "Collection Already Exists"
+            })
+
+        await userCollection.create({
+            collectionOwner: req?.user?.id,
+            collectionName: collection
+        })
+
+        return res.status(200).json({
+            msg: "collection created"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: "error creating collection",
+            err: error
+        })
+    }
+}
+
+export const getAllBlogsInACollection = async (req, res) => {
+    try {
+        const { collectionName } = req.params
+
+        const allBlogsInThisCollection = await userCollection.findOne({
+            collectionName: collectionName
+        }).populate('collectionChildrens')
+
+        if (!allBlogsInThisCollection) {
+            return res.status(409).json({
+                msg: "Collection Does Not exist"
+            })
+        }
+
+        return res.status(200).json({
+            msg: "All Blogs in this Collection",
+            allBlogsInThisCollection
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: "error getting blogs in this collection"
+        })
+    }
+}
+
 export const newBlog = async (req, res) => {
-    const { title, content, tags } = req.body
+    let { title, content, tags, collection } = req.body
     if (!title)
         return res.status(409).json({
             msg: "Title Is Required"
@@ -142,76 +209,163 @@ export const newBlog = async (req, res) => {
         })
 
     const blogImageLocalPath = req.file?.path
-    if (!blogImageLocalPath)
-        return res.status(200).json({
-            msg: "Blog Image Path Invalid"
+    let blogImage = ''
+    if (blogImageLocalPath) {
+        blogImage = await uploadOnCloudinary(blogImageLocalPath)
+    }
+
+    if (collection === "") {
+        const soloCollection = await userCollection.findOne({
+            collectionName: "Solo"
         })
 
-    const blogImage = await uploadOnCloudinary(blogImageLocalPath)
+        if (!soloCollection) {
+            await userCollection.create({
+                collectionName: "Solo",
+                collectionOwner: req.user?.id
+            })
+        }
 
-    const blog = await Blog.create({
-        owner: req.user?._id,
-        image: blogImage.secure_url,
+        const blog = await Blog.create({
+            owner: req.user?.id,
+            image: blogImage.secure_url,
+            title,
+            content,
+            tags: Array(tags),
+            image_resource_type: blogImage.resource_type,
+            image_public_id: blogImage.public_id,
+            parentCollection: soloCollection?._id
+        })
+
+        userCollection.findOneAndUpdate({
+            collectionName: "Solo"
+        }, {
+            $push: {
+                collectionChildrens: blog?._id
+            }
+        })
+
+        return res.status(200).json({
+            msg: "Blog created successfully"
+        })
+
+    } else {
+        const availableCollection = await userCollection.findOne({
+            collectionOwner: req?.user?.id,
+            collectionName: collection
+        })
+
+        const blog = await Blog.create({
+            owner: req.user?.id,
+            image: blogImage.secure_url,
+            title,
+            content,
+            tags: Array(tags),
+            image_resource_type: blogImage.resource_type,
+            image_public_id: blogImage.public_id,
+            parentCollection: availableCollection?._id
+        })
+
+        await userCollection.findOneAndUpdate({
+            collectionOwner: req?.user?.id,
+            collectionName: collection,
+        }, {
+            $push: {
+                collectionChildrens: blog?._id
+            }
+        })
+    }
+
+    return res.status(200).json({
+        msg: "Blog created successfully"
+    })
+}
+
+export const createDraft = async (req, res) => {
+    const { title, tags, content, collection } = req.body
+    if (!title) {
+        return res.status(403).json({
+            msg: "Title is required"
+        })
+    }
+
+    if (!collection) {
+        return res.status(403).json({
+            msg: "collection is required"
+        })
+    }
+
+    const foundCollection = await userCollection.findOne({
+        collectionOwner: req.user?.id,
+        collectionName: collection
+    })
+
+    const draft = await Blog.create({
+        owner: req.user?.id,
         title,
         content,
         tags: Array(tags),
         image_resource_type: blogImage.resource_type,
         image_public_id: blogImage.public_id,
-        // image_type: blogImage.type,
+        parentCollection: foundCollection?._id,
     })
 
-    await User.findByIdAndUpdate(
-        req.user._id,
-        {
-            $push: {
-                blogs: blog._id
-            }
-        }
-    )
-
-    const blogInfo = await Blog.findById(blog._id).select("owner image title content tags").populate("owner", "username avatar")
-
     return res.status(200).json({
-        msg: "Blog created successfully",
-        blogInfo
+        msg: "Saved In Draft",
+        draft
     })
 }
 
 export const updateDetails = async (req, res) => {
     try {
         const { blogId } = req.params
-        const { title, content, tags } = req.body
-        const updatedInfo = {}
-        // const originalBlog = await Blog.findById(blogId)
-        const currentImage = await Blog.findOne({
-            _id: blogId,
-            owner: req.user._id
-        }).select("image_resource_type image_public_id")
+        let { title, content, tags, collection, previousCollection } = req.body
+        let updatedInfo = {}
 
-        if (title) updatedInfo.title = title
-        if (content) updatedInfo.content = content
-        if (tags) updatedInfo.tags = Array(tags)
+        if (title !== undefined && title !== "") updatedInfo.title = title
+        if (content !== undefined && content !== "") updatedInfo.content = content
+        if (tags !== undefined && tags !== "") updatedInfo.tags = Array.isArray(tags) ? tags : [tags]
 
+        let newCollection
+        if (previousCollection === collection)
+            newCollection = previousCollection
 
-        const newImagePath = req.file?.path
-        if (newImagePath) {
-            await deleteFromCloudinary(currentImage.image_public_id, currentImage.image_resource_type)
-            const newImageInfo = await uploadOnCloudinary(newImagePath)
-            updatedInfo.image = newImageInfo.secure_url
+        if (previousCollection !== collection) {
+            updatedInfo.collection = collection
+            await userCollection.findOneAndUpdate({
+                collectionName: previousCollection
+            }, {
+                $pull: {
+                    collectionChildrens: blogId
+                }
+            })
 
-            const updatedBlogInfo = await Blog.findOneAndUpdate({
-                _id: blogId,
-                owner: req.user._id
-            }, { $set: updatedInfo }, { new: true })
+            newCollection = await userCollection.findOneAndUpdate({
+                collectionName: collection
+            }, {
+                $push: {
+                    collectionChildrens: blogId
+                }
+            })
 
-            return res.json({
-                msg: "update details",
-                // originalBlog,
-                updatedBlogInfo
+            await Blog.findOneAndUpdate({
+                _id: blogId
+            }, {
+                parentCollection: newCollection?._id
             })
         }
 
-    } catch (error) {
+        const updatedBlogInfo = await Blog.findOneAndUpdate({
+            _id: blogId
+        }, { $set: updatedInfo }, { new: true })
+
+
+        return res.status(200).json({
+            msg: "updates successful",
+            updatedBlogInfo
+        })
+    }
+    catch (error) {
         return res.status(500).json({
             msg: "Error Updating Details",
             err: error.message
@@ -221,11 +375,41 @@ export const updateDetails = async (req, res) => {
 
 export const deletePost = async (req, res) => {
     try {
-        return res.json({
-            msg: "update details"
+        const { blogId } = req.params
+        const blogImage = await Blog.findOne({
+            _id: blogId,
+            owner: req.user._id
+        }).select("image_public_id image_resource_type")
+
+        if (blogImage) {
+            deleteFromCloudinary(blogImage.image_public_id, blogImage.image_resource_type)
+        }
+
+        await userCollection.findOneAndDelete({
+            collectionChildrens: blogId
+        })
+
+        const userHasThePostInHisLikes = await User.findOne({
+            likedBlogs: blogId
+        })
+
+        if (userHasThePostInHisLikes) {
+            await User.findByIdAndUpdate(req.user?.id, {
+                $pull: {
+                    likedBlogs: blogId
+                }
+            })
+        }
+
+        await Blog.findByIdAndDelete(blogId)
+
+        return res.status(200).json({
+            msg: "Deleted"
         })
     } catch (error) {
-
+        return res.status(500).json({
+            err: error.message
+        })
     }
 }
 
@@ -233,43 +417,48 @@ export const likeBlog = async (req, res) => {
     try {
         const { blogId } = req.params
         const isLiked = await User.findOne({
-            _id: req.user?._id,
+            _id: req.user?.id,
             likedBlogs: blogId
         })
 
         if (isLiked === null) {
-            await User.findByIdAndUpdate(req.user?._id, {
-                $push: {
-                    likedBlogs: blogId
-                }
-            })
             await Blog.findByIdAndUpdate(blogId, {
                 $inc: {
                     likes: 1
                 }
             })
 
-            const userlikes = await User.findById(req.user?._id).select('likedBlogs')
+            await User.findByIdAndUpdate(req.user?.id, {
+                $push: {
+                    likedBlogs: blogId
+                }
+            })
+
+            const blogLikes = await Blog.findById(blogId).select('likes')
             return res.json({
                 msg: "liked",
-                userlikes
+                blogLikes,
+                liked: true
             })
         } else {
-            await User.findByIdAndUpdate(req.user?._id, {
+            await User.findByIdAndUpdate(req.user?.id, {
                 $pull: {
                     likedBlogs: blogId
                 }
             })
+
             await Blog.findByIdAndUpdate(blogId, {
                 $inc: {
                     likes: -1
                 }
             })
-            const userlikes = await User.findById(req.user?._id).select('likedBlogs')
+
+            const blogLikes = await Blog.findById(blogId).select('likes')
 
             return res.status(200).json({
                 msg: "removed like",
-                userlikes
+                blogLikes,
+                liked: false
             })
         }
     } catch (error) {
@@ -282,10 +471,16 @@ export const likeBlog = async (req, res) => {
 
 export const userLikedBlogs = async (req, res) => {
     try {
-        const userLikedBlogs = await User.findById(req.user?._id).populate('likedBlogs').select("likedBlogs")
+        const blogs = await User.findById(req?.user?.id).populate({
+            path: 'likedBlogs',
+            populate: {
+                path: 'owner',
+                select: 'username'
+            }
+        }).select('-password -email')
         return res.status(200).json({
             msg: "your liked posts",
-            userLikedBlogs
+            blogs
         })
     } catch (error) {
         return res.status(500).json({
@@ -293,6 +488,23 @@ export const userLikedBlogs = async (req, res) => {
         })
     }
 
+}
+
+export const userCollections = async (req, res) => {
+    try {
+        const userCollections = await userCollection.find({
+            collectionOwner: req.user?.id,
+        }).select('collectionName')
+
+        return res.status(200).json({
+            msg: "user collections",
+            userCollections
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: "error getting user's collections"
+        })
+    }
 }
 
 export const logout = async (req, res) => {
@@ -307,6 +519,43 @@ export const logout = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             msg: "Error logging out",
+            err: error.message
+        })
+    }
+}
+
+export const formtest = async (req, res) => {
+    try {
+        const { title, content, tags } = req.body
+        console.log('title , content , tags', title, content, tags)
+        if (!title)
+            return res.status(409).json({
+                msg: "Title Is Required"
+            })
+        if (!content)
+            return res.status(409).json({
+                msg: "Content Is Required"
+            })
+        if (!tags)
+            return res.status(409).json({
+                msg: "Tags are Required"
+            })
+
+        const blogImageLocalPath = req.file?.path
+        if (!blogImageLocalPath)
+            return res.status(200).json({
+                msg: "Blog Image Path Invalid"
+            })
+
+        return res.status(200).json({
+            title,
+            content,
+            tags: Array(tags),
+            image: blogImageLocalPath
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg: "Error in form data check",
             err: error.message
         })
     }
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import { connectDb } from "./db/db.js"
 import userRoutes from "./routes/user.routes.js"
+import cors from "cors"
 
 dotenv.config()
 const PORT = process.env.PORT
 const app = express()
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
